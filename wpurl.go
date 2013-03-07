f7049999d4a3264aff8b529c95957cbdbe5e2a7b
Reed O'Brien <reed@koansys.com>
2013-03-06 23:36:32 -0500

    kill me

--- wpurl.go
+++ wpurl.go
@@ -25,18 +25,19 @@ func main() {
 	allurls := db.AllUrls()
 	result := db.Url{}
 	for i := 0; i < concurrency; i++ {
+		wg.Add(concurrency)
 		go func() {
-			job := <-jobs
-			fmt.Printf("Processed: %s %s\n", job.Path, job.Status)
-			err = resources.Insert(&job)
-			if err != nil {
-				log.Panic(err)
+			for job := range jobs {
+				fmt.Printf("Processed: %s %s\n", job.Path, job.Status)
+				err = resources.Insert(&job)
+				if err != nil {
+					log.Panic(err)
+				}
+				wg.Done()
 			}
-			wg.Done()
 		}()
 	}
 	for allurls.Next(&result) {
-		wg.Add(1)
 		jobs <- fetcher.Head(result.Url)
 	}
 	wg.Wait()
