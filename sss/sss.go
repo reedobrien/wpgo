0ab6b2bb95e64d9a4b5293e2247add62952a6f0f
Reed O'Brien <reed@koansys.com>
2013-03-07 21:39:07 -0500

    forgot to add S3 stuff

---
+++ sss/sss.go
@@ -0,0 +1,37 @@
+package sss
+
+import (
+	"launchpad.net/goamz/aws"
+	"launchpad.net/goamz/s3"
+	"log"
+	// "os"
+)
+
+var BucketName = "v-nasa.gov"
+var Region = aws.USEast
+
+// func main() {
+// 	creds, err := aws.EnvAuth()
+// 	if err != nil {
+// 		log.Println("Error with aws credentials: %v", err)
+// 		os.Exit(1)
+// 	}
+// 	// bucket, err := GetBucket(creds, Region, BucketName)
+// }
+
+// Get a handle onto an s3 bucket
+func GetBucket(creds aws.Auth, region aws.Region, bucket_name string) *s3.Bucket {
+	log.Println("Setting up s3 access")
+	sss := s3.New(creds, region)
+	bucket := sss.Bucket(bucket_name)
+	// _, err := bucket.Get("sad;lkfjls;k")
+	// Create the bucket if it doesn't exist.
+	// if err != nil {
+	// 	s3err, _ := err.(*s3.Error)
+	// 	if s3err.Code == "NoSuchBucket" {
+	// 		log.Println("Creating s3 bucket")
+	// 		bucket.PutBucket(s3.Private)
+	// 	}
+	// }
+	return bucket
+}
