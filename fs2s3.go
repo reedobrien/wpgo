79b5589563b9b90bb083fc8f6c64b8671adaf6f3
Reed O'Brien <reed@koansys.com>
2013-04-03 08:30:45 -0400

    checkpoint -- totally broken
    Needs to use the channel or at least pass the struct rather than the individual args.

--- fs2s3.go
+++ fs2s3.go
@@ -8,13 +8,18 @@ import (
 	"fmt"
 	flag "github.com/ogier/pflag"
 	"io/ioutil"
-	//"launchpad.net/goamz/s3"
+	"launchpad.net/goamz/s3"
 	"mime"
 	"os"
 	"path"
 	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
 	//"wp/sss"
 )
+
+const concurrency = 1000
 
 var node string
 var bucket = flag.StringP("bucket", "b", "", "Use the named bucket")
@@ -27,10 +32,20 @@ var recursive = flag.BoolP("recursive", "r", false, "Upload everything resursive
 // force upload even if file is older than uploaded version?
 // var encrypted = .... set  x-amz-server-side-encryption: true header
 
+type FileUpload struct {
+	ContentType string
+	Path        string
+	Bucket      *s3.Bucket
+}
+
 func main() {
-	var bucketname string
 	flag.Parse()
+	var bucketname string
+
+	uploads := make(chan FileUpload, 1)
+	waiter := &sync.WaitGroup{}
 	directory := flag.Arg(0)
+
 	fmt.Println("Bucket : %s", *bucket)
 	if *bucket == "" {
 		bucketname = *prefix + directory
@@ -38,30 +53,61 @@ func main() {
 		bucketname = *prefix + *bucket
 	}
 	fmt.Println("Bucket Name: ", bucketname)
-	// open s3 bucket here
-	//s3bucket := sss.GetBucket(sss.Auth(), sss.Region, bucketname)
-	// headers := map[string][]string{
-	// 	"If-Modified-Since": {"Sat, 30 Mar 2012 15:48:13 GMT"},
-	// }
-	// err = s3bucket.IfModifiedSince("mp4/AirSTAR.mp4", headers)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	//os.Exit(0)
-	err := filepath.Walk(directory, visit)
-	// if error push donesemaphore down the chan.
+	s3bucket := sss.GetBucket(sss.Auth(), sss.Region, bucketname)
+	err := filepath.Walk(directory, makeVisitor(uploads, s3bucket, waiter, *public))
+	waiter.Wait()
 	fmt.Printf("filepath.Walk() returned %v\n", err)
 }
 
-func visit(fpath string, f os.FileInfo, err error) error {
-	node := isfile(f)
-	if node {
-		contType := mime.TypeByExtension(path.Ext(fpath))
-		// create an uploadfiles struct and stick it in the channel
-		fmt.Printf("%s\nVisited file:%t\n md5: %x:\n Name: %s\n Content Type: %s\n\n",
-			fpath, node, md5sum(fpath), f.Name(), contType)
+func makeVisitor(uploads chan FileUpload, bucket *s3.Bucket, waiter *sync.WaitGroup, public *bool) func(string, os.FileInfo, error) error {
+	return func(fpath string, f os.FileInfo, err error) error {
+		node := isfile(f)
+		if node {
+			contType := mime.TypeByExtension(path.Ext(fpath))
+			if contType == "" {
+				contType = "binary/octet-stream"
+			}
+			fu := FileUpload{
+				ContentType: contType,
+				Path:        fpath,
+				Bucket:      bucket,
+			}
+			if runtime.NumGoroutine() > concurrency {
+				uploadFile(fpath, f, *public, nil)
+			} else {
+				waiter.Add(1)
+				go uploadFile(fpath, f, *public,
+					func() {
+						waiter.Done()
+					})
+			}
+			// create an uploadfiles struct and stick it in the channel
+			fmt.Printf("%s\nVisited file:%t\n md5: %x:\n Name: %s\n Content Type: %s\n\n",
+				fpath, node, md5sum(fpath), f.Name(), contType)
+		}
+		return nil
 	}
-	return nil
+}
+
+func uploadFile(fpath string, f os.FileInfo, bucket *s3.Bucket, public *bool, done func()) error {
+	if done != nil {
+		defer done()
+	}
+	remotePath := fpath[strings.Index(fpath, bucket.Name)-len(*bucket.Name)]
+	acl := s3.Private
+	if *public {
+		acl = s3.PublicRead
+	}
+	fh, err := os.Open(fpath)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+	fi, err := fh.Stat()
+	if err != nil {
+		return err
+	}
+	return bucket.PutReader(f.Name(), fh, fi.Size(), contType, acl)
 }
 
 //func (b *s3.Bucket) ifModifiedSince(path string, headers)
