8c414e03795e01973209c27f83a00d15ae52f8be
Reed O'Brien <reed@koansys.com>
2013-04-03 20:35:55 -0400

    checkpoint - might work
    Can't tell no net on train now.

--- fs2s3.go
+++ fs2s3.go
@@ -14,9 +14,8 @@ import (
 	"path"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"sync"
-	//"wp/sss"
+	"wp/sss"
 )
 
 const concurrency = 1000
@@ -25,8 +24,8 @@ var node string
 var bucket = flag.StringP("bucket", "b", "", "Use the named bucket")
 var prefix = flag.StringP("prefix", "x", "v-", "Set a prefix on the bucketname")
 var public = flag.BoolP("public", "p", false, "Makes the uploaded files publicly visible")
-var recursive = flag.BoolP("recursive", "r", false, "Upload everything resursively from the path")
 
+// var recursive = flag.BoolP("recursive", "r", false, "Upload everything resursively from the path")
 // verify sums?
 // guess mimetypes
 // force upload even if file is older than uploaded version?
@@ -45,21 +44,24 @@ func main() {
 	uploads := make(chan FileUpload, 1)
 	waiter := &sync.WaitGroup{}
 	directory := flag.Arg(0)
-
-	fmt.Println("Bucket : %s", *bucket)
 	if *bucket == "" {
 		bucketname = *prefix + directory
 	} else {
 		bucketname = *prefix + *bucket
 	}
-	fmt.Println("Bucket Name: ", bucketname)
+	fmt.Println("Uploading to bucket named: ", bucketname)
+	fmt.Println("Publicly visible:", *public)
 	s3bucket := sss.GetBucket(sss.Auth(), sss.Region, bucketname)
 	err := filepath.Walk(directory, makeVisitor(uploads, s3bucket, waiter, *public))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	waiter.Wait()
-	fmt.Printf("filepath.Walk() returned %v\n", err)
+	// fmt.Printf("filepatxh.Walk() returned %v\n", err)
 }
 
-func makeVisitor(uploads chan FileUpload, bucket *s3.Bucket, waiter *sync.WaitGroup, public *bool) func(string, os.FileInfo, error) error {
+func makeVisitor(uploads chan FileUpload, bucket *s3.Bucket, waiter *sync.WaitGroup, public bool) func(string, os.FileInfo, error) error {
 	return func(fpath string, f os.FileInfo, err error) error {
 		node := isfile(f)
 		if node {
@@ -73,32 +75,30 @@ func makeVisitor(uploads chan FileUpload, bucket *s3.Bucket, waiter *sync.WaitGr
 				Bucket:      bucket,
 			}
 			if runtime.NumGoroutine() > concurrency {
-				uploadFile(fpath, f, *public, nil)
+				uploadFile(fu, public, nil)
 			} else {
 				waiter.Add(1)
-				go uploadFile(fpath, f, *public,
+				go uploadFile(fu, public,
 					func() {
 						waiter.Done()
 					})
 			}
-			// create an uploadfiles struct and stick it in the channel
-			fmt.Printf("%s\nVisited file:%t\n md5: %x:\n Name: %s\n Content Type: %s\n\n",
-				fpath, node, md5sum(fpath), f.Name(), contType)
+			// fmt.Printf("%s\nVisited file:%t\n md5: %x:\n Name: %s\n Content Type: %s\n\n",
+			// 	fpath, node, md5sum(fpath), f.Name(), contType)
 		}
 		return nil
 	}
 }
 
-func uploadFile(fpath string, f os.FileInfo, bucket *s3.Bucket, public *bool, done func()) error {
+func uploadFile(fu FileUpload, public bool, done func()) error {
 	if done != nil {
 		defer done()
 	}
-	remotePath := fpath[strings.Index(fpath, bucket.Name)-len(*bucket.Name)]
 	acl := s3.Private
-	if *public {
+	if public {
 		acl = s3.PublicRead
 	}
-	fh, err := os.Open(fpath)
+	fh, err := os.Open(fu.Path)
 	if err != nil {
 		return err
 	}
@@ -107,7 +107,7 @@ func uploadFile(fpath string, f os.FileInfo, bucket *s3.Bucket, public *bool, do
 	if err != nil {
 		return err
 	}
-	return bucket.PutReader(f.Name(), fh, fi.Size(), contType, acl)
+	return fu.Bucket.PutReader(fu.Path, fh, fi.Size(), fu.ContentType, acl)
 }
 
 //func (b *s3.Bucket) ifModifiedSince(path string, headers)
