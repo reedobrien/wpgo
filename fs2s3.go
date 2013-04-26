91172c9bc26f81a920d5952cdceaaca6f00fe0ea
Reed O'Brien <reed@koansys.com>
2013-04-26 00:31:12 -0400

    add newer and newermetamtime
    This solves most of #2.
    
    However, I think newer should be the default. Should there be a flag to say don't upload newer than last modified?
    
    I think we might want to leave a flag for metamtime, though.

--- fs2s3.go
+++ fs2s3.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"launchpad.net/goamz/s3"
 	"mime"
+	"net/http"
 	"os"
 	"path"
 	"path/filepath"
@@ -28,8 +29,10 @@ var bucket = flag.StringP("bucket", "b", "", "Use the named bucket")
 var prefix = flag.StringP("prefix", "x", "v-", "Set a prefix on the bucketname")
 var public = flag.BoolP("public", "p", false, "Makes the uploaded files publicly visible")
 var force = flag.BoolP("force", "f", false, "Force upload regardless of existance or mtime")
-var mtime = flag.IntP("mtime", "m", 0, "Upload if newer than Last-Modified")
-var newer = flag.IntP("newer", "n", 0, "Upload if newer than x-amz-meta-last-modified")
+
+// XXX: Should this always be true? I think so.
+var newer = flag.BoolP("newer", "n", false, "Upload if file time is newer than Last-modified. Default: false")
+var newermetamtime = flag.BoolP("newwermetamtime", "N", false, "Upload if file is newer than x-amz-meta-last-modified. Default: false")
 
 // var recursive = flag.BoolP("recursive", "r", false, "Upload everything resursively from the path")
 // verify sums?
@@ -58,7 +61,7 @@ func main() {
 	fmt.Println("Uploading to bucket named: ", bucketname)
 	fmt.Println("Publicly visible:", *public)
 	s3bucket := sss.GetBucket(sss.Auth(), sss.Region, bucketname) // Should fold these into an options map/struct
-	err := filepath.Walk(directory, makeVisitor(uploads, s3bucket, waiter, *public, *force))
+	err := filepath.Walk(directory, makeVisitor(uploads, s3bucket, waiter, *public, *force, *newer, *newermetamtime))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -67,7 +70,7 @@ func main() {
 	// fmt.Printf("filepatxh.Walk() returned %v\n", err)
 }
 
-func makeVisitor(uploads chan FileUpload, bucket *s3.Bucket, waiter *sync.WaitGroup, public bool, force bool) func(string, os.FileInfo, error) error {
+func makeVisitor(uploads chan FileUpload, bucket *s3.Bucket, waiter *sync.WaitGroup, public, force, newer, newermetamtime bool) func(string, os.FileInfo, error) error {
 	return func(fpath string, f os.FileInfo, err error) error {
 		node := isfile(f)
 		if node {
@@ -81,22 +84,20 @@ func makeVisitor(uploads chan FileUpload, bucket *s3.Bucket, waiter *sync.WaitGr
 				Bucket:      bucket,
 			}
 			if runtime.NumGoroutine() > concurrency {
-				uploadFile(fu, public, force, nil)
+				uploadFile(fu, public, force, newer, newermetamtime, nil)
 			} else {
 				waiter.Add(1)
-				go uploadFile(fu, public, force,
+				go uploadFile(fu, public, force, newer, newermetamtime,
 					func() {
 						waiter.Done()
 					})
 			}
-			// fmt.Printf("%s\nVisited file:%t\n md5: %x:\n Name: %s\n Content Type: %s\n\n",
-			// 	fpath, node, md5sum(fpath), f.Name(), contType)
 		}
 		return nil
 	}
 }
 
-func uploadFile(fu FileUpload, public bool, force bool, done func()) error {
+func uploadFile(fu FileUpload, public, force, newer, newermetamtime bool, done func()) error {
 	if done != nil {
 		defer done()
 	}
@@ -123,11 +124,9 @@ func uploadFile(fu FileUpload, public bool, force bool, done func()) error {
 		}
 		return err
 	}
-
 	resp, err := fu.Bucket.Head(remotePath, nil)
+
 	if err != nil {
-		// TODO: this should maybe move into a shouldUpload func which would need a force flag.
-		// If force upload anyway otherwise test for modified since and decide
 		if e, ok := err.(*s3.Error); ok && e.StatusCode == 404 {
 			meta = map[string][]string{
 				"last-modified": {fi.ModTime().Format(time.RFC1123)},
@@ -140,9 +139,22 @@ func uploadFile(fu FileUpload, public bool, force bool, done func()) error {
 			}
 		}
 	} else {
-		fmt.Fprintln(os.Stderr, "Already uploaded:", remotePath, "at:", resp.Header["Last-Modified"])
+		if shouldUpdate(resp, fi, newer, newermetamtime) {
+			meta = map[string][]string{
+				"last-modified": {fi.ModTime().Format(time.RFC1123)},
+			}
+			if err := fu.Bucket.PutReaderWithMeta(remotePath, fh, fi.Size(), fu.ContentType, acl, meta); err != nil {
+				fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			} else {
+				// os.Exit(1)
+				fmt.Println("Uploaded:", remotePath, "Size:", fi.Size(), "content-type:", fu.ContentType)
+			}
+		} else {
+			fmt.Fprintln(os.Stderr, "Already uploaded:", remotePath, "Last modified:", resp.Header.Get("Last-Modified"))
+		}
+		return err
 	}
-	return err
+	return nil
 }
 
 //func (b *s3.Bucket) ifModifiedSince(path string, headers)
@@ -161,4 +173,32 @@ func isfile(f os.FileInfo) bool {
 		return false
 	}
 	return true
+}
+
+func shouldUpdate(resp *http.Response, fi os.FileInfo, newer, newermetamtime bool) bool {
+	var stale bool = false
+	filemtime := fi.ModTime()
+	if newermetamtime {
+		parsed, err := time.Parse(time.RFC1123, resp.Header.Get("x-amz-meta-last-modified"))
+		if err != nil {
+			// can't see metamtime upload anyhow
+			stale = true
+			fmt.Fprint(os.Stderr, "Can't read metamtime, setting stale to upload again\n")
+		}
+		if parsed.Before(filemtime) {
+			stale = true
+		}
+	}
+	if newer {
+		parsed, err := time.Parse(time.RFC1123, resp.Header.Get("Last-Modified"))
+		if err != nil {
+			// can't see metamtime upload anyhow
+			stale = true
+			fmt.Fprint(os.Stderr, "Can't read metamtime, setting stale to upload again\n")
+		}
+		if parsed.Before(filemtime) {
+			stale = true
+		}
+	}
+	return stale
 }
